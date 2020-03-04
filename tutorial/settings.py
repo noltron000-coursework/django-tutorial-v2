6bbd85e5206f36e19862024028976d77a74cf616
Nolan <noltron000@gmail.com>
2020-03-03 22:29:40 -0800

    [add] prepping heroku files

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e07dael@t+=^s15y8+o-%#3w&!k0r^(1#%6z239ll0^frpvip9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'tutorial.herokuapp.com']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
